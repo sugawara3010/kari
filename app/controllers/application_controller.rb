53d8e0fdaeaa4ff0359e9ea6ed12838dc78136d0
菅原駿 <sugawarashun@sugawarashunnonotobukkukonpyuta.local>
2024-08-16 17:52:41 +0900

    2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :require_login
+  helper_method :user_signed_in?
+
+  def user_signed_in?
+    !current_user.nil?
+  end
+
+  private
+
+  def not_authenticated
+    redirect_to login_path
+  end
 end
