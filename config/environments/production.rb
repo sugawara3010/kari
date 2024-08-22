58b237306fdded0d1c75821b7a8ff25af8110937
菅原駿 <sugawarashun@sugawarashunnonotobukkukonpyuta.local>
2024-08-22 17:38:00 +0900

    カレンダー

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,5 +99,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  [username@ip-10-0-11-63 test_app]$ export RAILS_SERVE_STATIC_FILES=true
 end
